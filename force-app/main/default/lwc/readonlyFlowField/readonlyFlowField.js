cdf32cbd2af1452e17550d4dd83e2ca5e70c8da0
David Fromstein <david.fromstein@salesforce.com>
2021-02-23 23:01:02 -0800

    tidying up

--- force-app/main/default/lwc/readonlyFlowField/readonlyFlowField.js
+++ force-app/main/default/lwc/readonlyFlowField/readonlyFlowField.js
@@ -1,10 +1,6 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getObjectApiName from '@salesforce/apex/ReadonlyFlowFieldController.getObjectApiName';
 
-const READONLY_MODE = 'readonly';
-const EDITABLE_MODE = 'edit';
-import { getRecord } from 'lightning/uiRecordApi';
-
 export default class ReadonlyFlowField extends LightningElement {
 
     @api recordId;
@@ -12,26 +8,10 @@ export default class ReadonlyFlowField extends LightningElement {
     @api isReadonly;
     objectName;
 
-    /*
-    get mode() {
-        return this.isReadonly ? READONLY_MODE : EDITABLE_MODE;
-    }
-    */
 
     connectedCallback() {
         getObjectApiName({recordId: this.recordId}).then(result => {
             this.objectName = result;
         })
     }
-
-    /*
-    @wire(getRecord, { recordId: '$recordId', fields: 'Id' })
-    getObjectName({error, data}) {
-        if (error)
-        console.log('getObjectName error: '+ JSON.stringify(error));
-        if (data) {
-            this.objectName = data.apiName;
-        }
-    }
-    */
 }
